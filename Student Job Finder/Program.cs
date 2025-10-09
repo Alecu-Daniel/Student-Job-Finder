2558e90216c4556843c8bc4c19bdc1d073c1d7e4
Dany <Dany@DESKTOP-Q79QAPR>
2025-10-09 19:22:06 +0300

    Database Connection + Database Context

--- Student Job Finder/Program.cs
+++ Student Job Finder/Program.cs
@@ -2,9 +2,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 
+//Swagger stuff
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 app.UseStaticFiles();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseRouting();
 app.UseEndpoints(endpoints =>
