596c52c20bf1a2526b7cbec4a16daf66b5089ce4
Dany <Dany@DESKTOP-Q79QAPR>
2025-10-09 14:29:54 +0300

    Add project files.

---
+++ Student Job Finder/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Student_Job_Finder.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
