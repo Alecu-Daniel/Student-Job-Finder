cbad91752a1405f0cad51ef8022640632e39d990
Dany <Dany@DESKTOP-Q79QAPR>
2025-10-11 22:36:54 +0300

    Implemented Authentication and Registering

---
+++ Student Job Finder/Controllers/AuthController.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Student_Job_Finder.Data;
+using Student_Job_Finder.Dtos;
+using Student_Job_Finder.Models;
+using System.Data;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Student_Job_Finder.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly DataContextDapper _dapper;
+        private readonly IConfiguration _config;
+        public AuthController(IConfiguration config)
+        {
+            _dapper = new DataContextDapper(config);
+            _config = config;
+        }
+
+        [HttpPost("Register")]
+        public IActionResult Register(UserForRegistrationDto userForRegistration)
+        {
+            if(userForRegistration.Password == userForRegistration.PasswordConfirm)
+            {
+                string sqlCheckUserExists = "SELECT Email FROM JobFinderSchema.Auth WHERE Email= '" +
+                    userForRegistration.Email + "'";
+                IEnumerable<string> existingUsers = _dapper.LoadData<string>(sqlCheckUserExists);
+                if (existingUsers.Count() == 0)
+                {
+                    byte[] passwordSalt = new byte[128 / 8];
+                    using (RandomNumberGenerator rng = RandomNumberGenerator.Create())
+                    {
+                        rng.GetNonZeroBytes(passwordSalt);
+                    }
+
+                    byte[] passwordHash = GetPasswordHash(userForRegistration.Password, passwordSalt);
+
+                    string sqlAddAuth = @"
+                        INSERT INTO JobFinderSchema.Auth ([Email],
+                        [PasswordHash],
+                        [PasswordSalt]) VALUES ('" + userForRegistration.Email +
+                    "' , @PasswordHash, @PasswordSalt)";
+
+
+                    List<SqlParameter> sqlParameters = new List<SqlParameter>();
+
+                    SqlParameter passwordSaltParameter = new SqlParameter("@PasswordSalt", SqlDbType.VarBinary);
+                    passwordSaltParameter.Value = passwordSalt;
+                    SqlParameter passwordHashParameter = new SqlParameter("@PasswordHash", SqlDbType.VarBinary);
+                    passwordHashParameter.Value = passwordHash;
+
+                    sqlParameters.Add(passwordSaltParameter);
+                    sqlParameters.Add(passwordHashParameter);
+
+                    if(_dapper.ExecuteSqlWithParameters(sqlAddAuth, sqlParameters))
+                    {
+
+                        string sqlAddUser = @"INSERT INTO JobFinderSchema.Users(
+                            [FirstName],
+                            [LastName],
+                            [Email],
+                            [Role]
+                        ) VALUES (" +
+                            "'" + userForRegistration.FirstName +
+                            "', '" + userForRegistration.LastName +
+                            "', '" + userForRegistration.Email +
+                            "', '" + userForRegistration.Role +
+                         "')";
+                        
+                        if(_dapper.ExecuteSql(sqlAddUser))
+                        {
+                            return Ok();
+                        }
+                        throw new Exception("Failed to Add user");
+                    }
+                    throw new Exception("Failed to Register user");
+                }
+                throw new Exception("User with this email already exists");
+            }
+            throw new Exception("Passwords do not match");
+
+        }
+
+        [HttpPost("Login")]
+        public IActionResult Login(UserForLoginDto userForLogin)
+        {
+            string sqlForHashAndSalt = @"SELECT
+                 [PasswordHash],
+                 [PasswordSalt] FROM JobFinderSchema.Auth WHERE Email = '" +
+                 userForLogin.Email + "'";
+
+            UserForLoginConfirmationDto userForConfirmation = _dapper
+                .LoadDataSingle<UserForLoginConfirmationDto>(sqlForHashAndSalt);
+
+            byte[] passwordHash = GetPasswordHash(userForLogin.Password, userForConfirmation.PasswordSalt);
+
+            for (int i = 0; i < passwordHash.Length; i++)
+            {
+                if(passwordHash[i] != userForConfirmation.PasswordHash[i])
+                {
+                    return StatusCode(401,"Incorrect password");
+                }
+            }
+            
+            return Ok();
+        }
+
+        private byte[] GetPasswordHash(string password, byte[] passwordSalt)
+        {
+            string passwordSaltPlusString = _config.GetSection("AppSettings:PasswordKey").Value +
+                                                    Convert.ToBase64String(passwordSalt);
+
+            return KeyDerivation.Pbkdf2(
+                    password: password,
+                    salt: Encoding.ASCII.GetBytes(passwordSaltPlusString),
+                    prf: KeyDerivationPrf.HMACSHA256,
+                    iterationCount: 1000000,
+                    numBytesRequested: 256 / 8
+                );
+        }
+    }
+}
---
+++ Student Job Finder/Dtos/UserForLoginConfirmationDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Student_Job_Finder.Dtos
+{
+    public class UserForLoginConfirmationDto
+    {
+        public byte[] PasswordHash { get; set; } = new byte[0];
+        public byte[] PasswordSalt { get; set; } = new byte[0];
+    }
+}
