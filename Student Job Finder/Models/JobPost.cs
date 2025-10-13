99ed4291d4c5a1c2d29bb8bfac8c8e20cd889d23
Dany <Dany@DESKTOP-Q79QAPR>
2025-10-13 20:28:02 +0300

    Implemented Job posts + search

---
+++ Student Job Finder/Models/JobPost.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Student_Job_Finder.Models
+{
+    public class JobPost
+    {
+        public int PostId { get; set; }
+        public int UserId { get; set; }
+        public string PostTitle { get; set; } = "";
+        public string PostContent { get; set; } = "";
+        public DateTime PostCreated { get; set; }
+        public DateTime PostUpdated { get; set; }
+    }
+}
