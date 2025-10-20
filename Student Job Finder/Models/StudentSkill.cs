0c9cd3225bd6e334a7deadbd1bd44c7d5512910e
Dany <Dany@DESKTOP-Q79QAPR>
2025-10-20 18:19:10 +0300

    Added StudentSkill Controller

---
+++ Student Job Finder/Models/StudentSkill.cs
@@ -0,0 +1,10 @@
+﻿namespace Student_Job_Finder.Models
+{
+    public class StudentSkill
+    {
+        public int StudentSkillId { get; set; }
+        public int StudentId { get; set; }
+        public string SkillName { get; set; } = "";
+        public decimal SkillScore { get; set; }
+    }
+}
