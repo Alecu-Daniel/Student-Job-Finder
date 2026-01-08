cd360ba9ff1f8099f3ed1c69ae8182ca02f228cb
Dany <Dany@DESKTOP-Q79QAPR>
2026-01-08 13:03:45 +0200

    Implemented skills for job posts , along with minimal frontend.

---
+++ Student Job Finder/Dtos/JobSkillToAddDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Student_Job_Finder.Dtos
+{
+    public class JobSkillToAddDto
+    {
+        public int JobPostId { get; set; }
+        public string SkillName { get; set; } = "";
+        public string SkillLevel { get; set; } = "";
+    }
+}
